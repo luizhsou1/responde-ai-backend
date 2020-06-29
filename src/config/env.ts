b91bcaf0cba0d40760bbe85bc3d7c25d2e414100
Luiz Henrique <luihsou1@gmail.com>
2020-06-28 23:20:11 -0300

    alterando portas do container, para evitar conflito com outros containers em execucao, quando forem baixar as imagens

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,5 +1,5 @@
 export default {
   mongoUrl: process.env.MONGO_URL || 'mongodb://mongo:27017/respondeAi',
-  port: (process.env.PORT || 3000) as number,
+  port: (process.env.PORT || 3030) as number,
   pathFirebaseJson: process.env.PATH_FIREBASE_JSON || '../../responde-ai-cloud-firebase-adminsdk-jw7h4-5eb2cc0c57.json',
 };
